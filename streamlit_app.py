39b07e9d989ef5d728dd839658c755014d43de3f
Hasan-msg <152963688+Hasan-msg@users.noreply.github.com>
2024-08-13 15:45:46 +0000

    pip install --upgrade pip wheel

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,13 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+from shiny.express import ui
+
+ui.page_opts(title = "Title")
+
+with ui.sidebar():
+    "Sidebar (input)"
+
+"Main content (output)"
